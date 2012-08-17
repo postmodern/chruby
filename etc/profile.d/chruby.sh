cbd21833975a3b92ac26b6d499015321135037db
Postmodern <postmodern.mod3@gmail.com>
2012-08-17 00:51:07 -0700

    Style.

--- etc/profile.d/chruby.sh
+++ etc/profile.d/chruby.sh
@@ -5,7 +5,6 @@ function chruby_reset()
 	[[ -z "$RUBY_PATH" ]] && return
 
 	export PATH=`sed -e "s|$RUBY_PATH\/[^:]*:||g; s|$HOME\/.gem\/[^:]*:||g" <<< $PATH` && hash -r
-
 	unset RUBYOPT
 	unset GEM_HOME
 	unset GEM_PATH
