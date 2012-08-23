78766e70a287d8e284ab03130c49597da29214ae
Postmodern <postmodern.mod3@gmail.com>
2012-08-22 23:22:07 -0700

    Added basic tests for chruby_use.

---
+++ test/chruby_use_test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+. test/helper.sh
+
+function test_chruby_use()
+{
+	local ruby_root="/usr/local/ruby-1.9.3-p194"
+
+	chruby_use $ruby_root
+
+	assertEquals "invalid RUBY" "$ruby_root" "$RUBY"
+	assertEquals "invalid RUBY_ENGINE"  "ruby" "$RUBY_ENGINE"
+	assertEquals "invalid RUBY_VERSION" "1.9.3" "$RUBY_VERSION"
+	assertEquals "invalid GEM_HOME" "$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION" "$GEM_HOME"
+	assertEquals "invalid GEM_PATH" "$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION:$RUBY/lib/ruby/gems/1.9.1" "$GEM_PATH"
+
+	assertEquals "could not find ruby in $PATH" "$ruby_root/bin/ruby" `which ruby`
+}
