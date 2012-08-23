41c0bc369dd6858aa4838b766be7bebb723c6d9f
Postmodern <postmodern.mod3@gmail.com>
2012-08-23 01:36:37 -0700

    Added tests for the main chruby function.

---
+++ test/chruby_test.sh
@@ -0,0 +1,23 @@
+#!/usr/share/shunit2/shunit2
+
+. test/helper.sh
+
+function tearDown()
+{
+	chruby_reset
+}
+
+function test_chruby_1_9()
+{
+	chruby "1.9"
+
+	assertEquals "did not match $TEST_RUBY with 1.9" "$TEST_RUBY" "$RUBY"
+}
+
+function test_chruby_system()
+{
+	chruby "$TEST_RUBY_VERSION"
+	chruby system
+
+	assertNull "did not reset the Ruby" "$RUBY"
+}
