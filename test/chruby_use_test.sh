19ab032a6ef32065ebd7f1a82b674a6a1433674b
Postmodern <postmodern.mod3@gmail.com>
2012-08-22 23:31:38 -0700

    Improved chruby_use test.

--- test/chruby_use_test.sh
+++ test/chruby_use_test.sh
@@ -11,8 +11,9 @@ function test_chruby_use()
 	assertEquals "invalid RUBY" "$ruby_root" "$RUBY"
 	assertEquals "invalid RUBY_ENGINE"  "ruby" "$RUBY_ENGINE"
 	assertEquals "invalid RUBY_VERSION" "1.9.3" "$RUBY_VERSION"
+	assertEquals "invalid GEM_ROOT" "$RUBY/lib/ruby/gems/1.9.1" "$GEM_ROOT"
 	assertEquals "invalid GEM_HOME" "$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION" "$GEM_HOME"
-	assertEquals "invalid GEM_PATH" "$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION:$RUBY/lib/ruby/gems/1.9.1" "$GEM_PATH"
+	assertEquals "invalid GEM_PATH" "$GEM_HOME:$GEM_ROOT" "$GEM_PATH"
 
 	assertEquals "could not find ruby in $PATH" "$ruby_root/bin/ruby" `which ruby`
 }
