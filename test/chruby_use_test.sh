73d1760d52f7bc957c1e8b85910b89d385cb7b83
Postmodern <postmodern.mod3@gmail.com>
2012-08-22 23:41:46 -0700

    Fixed tests.

--- test/chruby_use_test.sh
+++ test/chruby_use_test.sh
@@ -7,10 +7,10 @@ function test_chruby_use()
 	chruby_use $TEST_RUBY
 
 	assertEquals "invalid RUBY" "$TEST_RUBY" "$RUBY"
-	assertEquals "invalid RUBY_ENGINE"  "ruby" "$RUBY_ENGINE"
-	assertEquals "invalid RUBY_VERSION" "1.9.3" "$RUBY_VERSION"
-	assertEquals "invalid GEM_ROOT" "$RUBY/lib/ruby/gems/$TEST_RUBY_API" "$GEM_ROOT"
-	assertEquals "invalid GEM_HOME" "$HOME/.gem/$RUBY_ENGINE/$RUBY_VERSION" "$GEM_HOME"
+	assertEquals "invalid RUBY_ENGINE"  "$TEST_RUBY_ENGINE" "$RUBY_ENGINE"
+	assertEquals "invalid RUBY_VERSION" "$TEST_RUBY_VERSION" "$RUBY_VERSION"
+	assertEquals "invalid GEM_ROOT" "$TEST_RUBY/lib/ruby/gems/$TEST_RUBY_API" "$GEM_ROOT"
+	assertEquals "invalid GEM_HOME" "$HOME/.gem/$TEST_RUBY_ENGINE/$TEST_RUBY_VERSION" "$GEM_HOME"
 	assertEquals "invalid GEM_PATH" "$GEM_HOME:$GEM_ROOT" "$GEM_PATH"
 
 	assertEquals "could not find ruby in $PATH" "$TEST_RUBY/bin/ruby" `which ruby`
