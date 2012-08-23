be9c4c0999bb11eb6860294a24b7ccf7e0cd3be6
Postmodern <postmodern.mod3@gmail.com>
2012-08-23 01:20:42 -0700

    Style.

--- test/helper.sh
+++ test/helper.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 . ./etc/profile.d/chruby.sh
+RUBIES=($TEST_RUBY)
 
 chruby_reset
-
 TEST_PATH="$PATH"
 TEST_RUBY_ENGINE="ruby"
 TEST_RUBY_VERSION="1.9.3"
 TEST_RUBY_PATCHLEVEL="194"
 TEST_RUBY_API="1.9.1"
 TEST_RUBY="/usr/local/ruby-$TEST_RUBY_VERSION-p$TEST_RUBY_PATCHLEVEL"
-
-RUBIES=($TEST_RUBY)
