79e83a7b4ee7b108a7d8bb2f708fd36234462390
Postmodern <postmodern.mod3@gmail.com>
2012-11-16 02:16:50 -0800

    Attempt to detect RVM/rbenv/rbfu and give example migration config.

--- homebrew/chruby.rb
+++ homebrew/chruby.rb
@@ -11,18 +11,42 @@ class Chruby < Formula
     system 'make', 'install', "PREFIX=#{prefix}"
   end
 
-  def caveats; <<-EOS.undent
+  def caveats
+    alternatives = {
+      'RVM'   => '~/.rvm/rubies',
+      'rbenv' => '~/.rbenv/rubies',
+      'rbfu'  => '~/.rbfu/rubies'
+    }
+
+    ruby_manager, rubies_dir = alternatives.find do |name,dir|
+      File.directory?(File.expand_path(dir))
+    end
+
+    message = %{
     Add chruby to ~/.bashrc or ~/.profile:
 
       . #{prefix}/share/chruby/chruby.sh
+    }
 
+    if rubies_dir
+      message << %{
+      RUBIES=#{ruby_dir}/*
+      }
+    else
+      message << %{
       RUBIES=(
-        /usr/local/ruby-1.9.3
-        /opt/jruby-1.7.0
+        /usr/local/ruby-1.9.3-p327
+        /usr/local/jruby-1.7.0
+        /usr/local/rubinius-2.0.0-rc1
       )
+      }
+    end
 
+    message << %{
     For system-wide installation, add the above text to /etc/profile.
 
-    EOS
+    }
+
+    return message.undent
   end
 end
