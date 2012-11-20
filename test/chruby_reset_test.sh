dcda077a1c8d18901a693f67e0ed47cd5a6cd6ef
Postmodern <postmodern.mod3@gmail.com>
2012-11-19 20:14:52 -0800

    Test chruby_reset when sanitizing a PATH containing duplicate bin dirs.

--- test/chruby_reset_test.sh
+++ test/chruby_reset_test.sh
@@ -13,17 +13,27 @@ setUp()
 	export PATH="$GEM_HOME/bin:$GEM_ROOT/bin:$RUBY/bin:$TEST_PATH"
 
 	chruby_use "$TEST_RUBY"
-	chruby_reset
 }
 
 test_chruby_reset()
 {
+	chruby_reset
+
 	assertNull "RUBY was not unset"          "$RUBY"
 	assertNull "RUBY_ENGINE was not unset"   "$RUBY_ENGINE"
 	assertNull "RUBY_VERSION was not unset"  "$RUBY_VERSION"
 	assertNull "RUBYOPT was not unset"       "$RUBYOPT"
 	assertNull "GEM_HOME was not unset"      "$GEM_HOME"
 	assertNull "GEM_PATH was not unset"      "$GEM_PATH"
+
+	assertEquals "PATH was not sanitized"    "$TEST_PATH" "$PATH"
+}
+
+test_chruby_reset_duplicate_path()
+{
+	export PATH="$PATH:$GEM_HOME/bin:$GEM_ROOT/bin:$RUBY/bin"
+
+	chruby_reset
 
 	assertEquals "PATH was not sanitized"    "$TEST_PATH" "$PATH"
 }
