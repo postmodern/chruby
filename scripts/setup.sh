bcbc27ff4a8d7eacab94d3109b8d6fb2e90f81cc
Postmodern <postmodern.mod3@gmail.com>
2012-12-10 18:46:55 -0800

    Began refactoring setup.sh (issue #31).

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,77 +1,171 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-export PREFIX="/usr/local"
-export SRC_DIR="$PREFIX/src"
-
-CHRUBY_VERSION="0.2.1"
-
-RUBY_BUILD_VERSION="20121110"
-RUBY_BUILD="$PREFIX/bin/ruby-build"
-
+#
+# Constants
+#
 MRI_VERSION="1.9.3-p327"
-MRI_PATH="$PREFIX/ruby-$MRI_VERSION"
-
 JRUBY_VERSION="1.7.0"
-JRUBY_PATH="$PREFIX/jruby-$JRUBY_VERSION"
-
 RUBINIUS_VERSION="2.0.0-rc1"
-RUBINIUS_PATH="$PREFIX/rubinius-$RUBINIUS_VERSION"
 
+[[ -z "$PREFIX"  ]] && export PREFIX="/usr/local"
+[[ -z "$SRC_DIR" ]] && export SRC_DIR="$PREFIX/src"
+
+#
+# Functions
+#
 function log() {
 	if [[ -t 1 ]]; then echo -e "\e[1m\e[32m>>>\e[0m \e[1m\e[37m$1\e[0m"
 	else                echo ">>> $1"
 	fi
 }
 
-function install_ruby() {
-	log "Installing $1 into $2 ..."
-	sudo env RUBY_BUILD_BUILD_PATH=$SRC_DIR $RUBY_BUILD -k $1 $2
+function error() {
+	if [[ -t 1 ]]; then echo -e "!!! $1" >&2
+	else		    echo "!!! $1" >&2
+	fi
 }
 
-if [[ $(type -t brew) == "file" ]]; then
-	# OS X
-	brew install ruby-build --without-rbenv
-	brew install https://raw.github.com/postmodern/chruby/master/homebrew/chruby.rb
-else
-	# *nix
-	log "Downloading ruby-build $RUBY_BUILD_VERSION ..."
-	sudo wget https://github.com/sstephenson/ruby-build/archive/v$RUBY_BUILD_VERSION.tar.gz -O $SRC_DIR/ruby-build-$RUBY_BUILD_VERSION.tar.gz
-	sudo tar -C $SRC_DIR/ -xzvf $SRC_DIR/ruby-build-$RUBY_BUILD_VERSION.tar.gz
-	log "Installing ruby-build $RUBY_BUILD_VERSION ..."
-	sudo sh -c "cd $SRC_DIR/ruby-build-$RUBY_BUILD_VERSION/ && ./install.sh"
+#
+# Detect the Package Manager
+#
+if   [[ $(type -t apt-get) == "file" ]]; then PACKAGE_MANAGER="apt"
+elif [[ $(type -t yum)     == "file" ]]; then PACKAGE_MANAGER="yum"
+elif [[ $(type -t brew)    == "file" ]];
+	PACKAGE_MANAGER="homebrew"
 
-	log "Downloading chruby-$CHRUBY_VERSION ..."
-	sudo wget https://github.com/downloads/postmodern/chruby/chruby-$CHRUBY_VERSION.tar.gz -O $SRC_DIR/chruby-$CHRUBY_VERSION.tar.gz
-	sudo tar -C $SRC_DIR/ -xzvf $SRC_DIR/chruby-$CHRUBY_VERSION.tar.gz
-	log "Installing chruby-$CHRUBY_VERSION ..."
-	sudo make -C $SRC_DIR/chruby-$CHRUBY_VERSION/ install
+	if [[ $(type -t cc) != "file" ]] || [[ $(type -t cpp) != "file" ]]; then
+		error "Could not find a C/C++ compiler"
+		error "Please install Command Line Tools:"
+		error "  https://developer.apple.com/downloads/index.action"
+		exit 1
+	fi
 fi
 
-install_ruby "$MRI_VERSION" "$MRI_PATH"
-install_ruby "jruby-$JRUBY_VERSION" "$JRUBY_PATH"
-install_ruby "rbx-$RUBINIUS_VERSION" "$RUBINIUS_PATH"
+#
+# Install chruby
+#
+log "Installing chruby ..."
+make install
+
+#
+# Install MRI (https://github.com/postmodern/chruby/wiki/MRI)
+#
+log "Installing dependencies for Ruby ..."
+
+case "$PACKAGE_MANAGER" in
+	apt) apt-get install -y build-essential zlib1g-dev libyaml-dev \
+			        libssl-dev libgdbm-dev libreadline-dev \
+				libncurses5-dev libffi-dev ;;
+	yum) yum install -y gcc automake zlib-devel libyaml-devel \
+			    openssl-devel gdbm-devel readline-devel \
+			    ncurses-devel libffi-devel ;;
+	homebrew) brew install libyaml gdbm libffi ;;
+esac
+
+cd $SRC_DIR
+
+log "Downloading Ruby $MRI_VERSION ..."
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-$MRI_VERSION.tar.gz
+
+log "Extracting Ruby $MRI_VERSION ..."
+tar -xzvf ruby-$MRI_VERSION.tar.gz
+cd ruby-$MRI_VERSION
+
+log "Configuring Ruby $MRI_VERSION ..."
+./configure --prefix=$PREFIX/ruby-$MRI_VERSION
+
+log "Compiling Ruby $MRI_VERSION ..."
+make
+
+log "Installing Ruby $MRI_VERSION ..."
+make install
+
+#
+# Install JRuby (https://github.com/postmodern/chruby/wiki/JRuby)
+#
+
+log "Installing dependencies for JRuby ..."
+case "$PACKAGE_MANAGER" in
+	apt)	apt-get install -y openjdk-7-jdk ;;
+	yum)	yum install -y java-1.7.0-openjdk ;;
+	homebrew)
+		# prompt user to install OracleJDK
+		;;
+esac
+
+cd $SRC_DIR
+
+log "Downloading JRuby $JRUBY_VERSION ..."
+wget http://jruby.org.s3.amazonaws.com/downloads/$JRUBY_VERSION/jruby-bin-$JRUBY_VERSION.tar.gz
+
+log "Installing JRuby $JRUBY_VERSION ..."
+tar -xzvf jruby-bin-$JRUBY_VERSION.tar.gz -C $PREFIX
+ln -s jruby $PREFIX/jruby-$JRUBY_VERSION/bin/ruby
+
+#
+# Install Rubinius (https://github.com/postmodern/chruby/wiki/Rubinius)
+#
+
+log "Installing dependencies for Rubinius ..."
+case "$PACKAGE_MANAGER" in
+	apt)
+		# Debian
+		apt-get install -y gcc g++ automake flex bison ruby-dev rake \
+		zlib1g-dev libyaml-dev libssl-dev libgdbm-dev libreadline-dev \
+		libncurses5-dev
+
+		# Ubuntu
+		apt-get install -y gcc g++ automake flex bison ruby-dev rake \
+			           llvm-3.0-dev zlib1g-dev libyaml-dev \
+				   libssl-dev libgdbm-dev libreadline-dev \
+				   libncurses5-dev
+		update-alternatives --install /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-3.0 30
+		;;
+	yum)	yum install -y gcc gcc-c++ automake flex bison ruby-devel \
+		               rubygems rubygem-rake llvm-devel zlib-devel \
+			       libyaml-devel openssl-devel gdbm-devel \
+			       readline-devel ncurses-devel ;;
+	homebrew)	brew install libyaml gdbm ;;
+esac
+
+log "Downloading Rubinius $RUBINIUS_VERSION ..."
+wget -O rubinius-release-$RUBINIUS_VERSION.tar.gz https://github.com/rubinius/rubinius/archive/release-$RUBINIUS_VERSION.tar.gz
+
+log "Extracting Rubinius $RUBINIUS_VERSION ..."
+tar -xzvf rubinius-release-$RUBINIUS_VERSION.tar.gz
+cd rubinius-release-$RUBINIUS_VERSION
+
+log "Configuring Rubinius $RUBINIUS_VERSION ..."
+./configure --prefix=/usr/local/rubinius-$RUBINIUS_VERSION
+
+log "Compiling Rubinius $RUBINIUS_VERSION ..."
+rake
+
+log "Installing Rubinius $RUBINIUS_VERSION ..."
+rake install
 
 CHRUBY_CONFIG=`cat <<EOS
 #!/bin/sh
 
-. /usr/local/share/chruby/chruby.sh
+. $PREFIX/share/chruby/chruby.sh
 
 RUBIES=(
-  $MRI_PATH
-  $JRUBY_PATH
-  $RUBINIUS_PATH
+  $PREFIX/ruby-$MRI_VERSION
+  $PREFIX/jruby-$JRUBY_VERSION
+  $PREFIX/rubinius-$RUBINIUS_VERSION
 )
 EOS`
 
 log "Configuring chruby ..."
+
 if [[ -d /etc/profile.d/ ]]; then
 	# GNU/Linux
-	sudo sh -c "echo \"$CHRUBY_CONFIG\" > /etc/profile.d/chruby.sh"
+	echo "$CHRUBY_CONFIG" > /etc/profile.d/chruby.sh
 else
 	# *nix
-	sudo sh -c "echo \"$CHRUBY_CONFIG\" >> /etc/profile"
+	echo "$CHRUBY_CONFIG" >> /etc/profile
 fi
 
 log "Setup complete! Please restart the shell"
