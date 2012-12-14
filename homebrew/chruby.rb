20ab3ddb7e794adf1daf1d3edd948221c33604e1
Postmodern <postmodern.mod3@gmail.com>
2012-12-13 22:32:57 -0800

    Updated the homebrew recipe for 0.2.4.

--- homebrew/chruby.rb
+++ homebrew/chruby.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Chruby < Formula
 
   homepage 'https://github.com/postmodern/chruby#readme'
-  url 'https://github.com/downloads/postmodern/chruby/chruby-0.2.3.tar.gz'
-  sha1 '6b8337ba2f48d3bbae680031b342680f39673af0'
+  url 'https://github.com/downloads/postmodern/chruby/chruby-0.2.4.tar.gz'
+  sha1 'c89d4a4732d63515b1948a3d0dd0e3a2d4ed1057'
   head 'https://github.com/postmodern/chruby.git'
 
   def install
