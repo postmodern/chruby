1951610c64e041b51f206b995a5299a71cd0cc5e
Postmodern <postmodern.mod3@gmail.com>
2012-12-13 23:45:45 -0800

    Fixed typo.

--- test/helper.sh
+++ test/helper.sh
@@ -3,7 +3,6 @@
 [[ -z "$SHUNIT2"     ]] && SHUNIT2=/usr/share/shunit2/shunit2
 [[ -n "$ZSH_VERSION" ]] && setopt shwordsplit
 
-<<<<<<< HEAD
 . ./share/chruby/chruby.sh
 export PATH="$PWD/bin:$PATH"
 
