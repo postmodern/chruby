c0238d89d43ee45e5a627197f458f262e507ffc1
Postmodern <postmodern.mod3@gmail.com>
2012-12-14 00:07:31 -0800

    Added tests for chruby_auto (issue #40).
    * Due to PROMPT_COMMAND and precmd not being called in non-interactive
      mode, I had to explicitly call chruby_auto after every cd invocation.

---
+++ test/chruby_auto_test.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+. ./share/chruby/auto.sh
+. ./test/helper.sh
+
+PROJECT_DIR="$PWD/test/project"
+
+setUp()
+{
+	chruby_reset
+}
+
+test_chruby_auto_enter_dir()
+{
+	cd "$PROJECT_DIR" && chruby_auto
+
+	assertEquals "did not switch Ruby when entering a versioned directory" \
+		     "$TEST_RUBY" "$RUBY"
+}
+
+test_chruby_auto_enter_subdir()
+{
+	cd "$PROJECT_DIR" && chruby_auto
+	cd dir1/dir2      && chruby_auto
+
+	assertEquals "did not keep the current Ruby when entering a sub-dir" \
+		     "$TEST_RUBY" "$RUBY"
+}
+
+test_chruby_auto_leave_dir()
+{
+	cd "$PROJECT_DIR" && chruby_auto
+	cd dir1/dir2
+	cd ../../..       && chruby_auto
+
+	assertNull "did not reset the Ruby when leaving a versioned directory" \
+		   "$RUBY"
+}
+
+SHUNIT_PARENT=$0 . $SHUNIT2
