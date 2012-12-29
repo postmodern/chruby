6cd7bc46e20a338ad333bc5130f81ba394d86444
Postmodern <postmodern.mod3@gmail.com>
2012-12-29 01:24:27 -0800

    Fixed argument sequence.

--- test/chruby_test.sh
+++ test/chruby_test.sh
@@ -26,7 +26,7 @@ test_chruby_unknown()
 {
 	chruby "foo" 2>/dev/null
 
-	assertEquals "did not return 1" $? 1
+	assertEquals "did not return 1" 1 $?
 }
 
 test_chruby_invalid_ruby()
@@ -35,7 +35,7 @@ test_chruby_invalid_ruby()
 
 	chruby "jruby" 2>/dev/null
 
-	assertEquals "did not return 1" $? 1
+	assertEquals "did not return 1" 1 $?
 }
 
 SHUNIT_PARENT=$0 . $SHUNIT2
