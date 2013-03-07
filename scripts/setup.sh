924aa7a86d6ee5bc19a12e213ed1d435cff0b5f9
Postmodern <postmodern.mod3@gmail.com>
2013-03-07 02:58:00 -0800

    Bump jruby version to 1.7.3
    Bumped JRUBY_VERSION to 1.7.3

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,7 +10,7 @@ set -e
 # Constants
 #
 RUBY_VERSION="1.9.3-p392"
-JRUBY_VERSION="1.7.2"
+JRUBY_VERSION="1.7.3"
 RUBINIUS_VERSION="2.0.0-rc1"
 
 [[ -z "$PREFIX"      ]] && export PREFIX="/usr/local"
