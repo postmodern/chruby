9eeb04e3c8ac4d0058824266d3e396d469a55f2f
Ravi Gadad <ravi@renewfund.com>
2013-04-06 13:51:24 -0700

    chruby-exec uses .ruby-version if no RUBY spec'd
    If chruby-exec is called with a command but no RUBY specified (before
    the "--" delimiter), it will attempt to locate a .ruby-version file
    in the current working directory or its ancestors (just as chruby_auto
    does).

--- test/chruby_exec_test.sh
+++ test/chruby_exec_test.sh
@@ -1,5 +1,7 @@
 . ./test/helper.sh
 
+PROJECT_DIR="$PWD/test/project"
+
 test_chruby_exec_no_arguments()
 {
 	chruby-exec 2>/dev/null
@@ -12,6 +14,22 @@ test_chruby_exec_no_command()
 	chruby-exec "$TEST_RUBY_VERSION" 2>/dev/null
 
 	assertEquals "did not exit with 1" 1 $?
+}
+
+test_chruby_exec_no_ruby_or_ruby_version_file()
+{
+	local command="ruby -e 'print RUBY_VERSION'"
+	chruby-exec -- '$command' 2>/dev/null
+
+	assertEquals "did not exit with 1" 1 $?
+}
+
+test_chruby_exec_no_ruby_but_with_ruby_version_file()
+{
+	local command="echo \$RUBY_ROOT"
+	local ruby_root=$(cd $PROJECT_DIR && chruby-exec -- $command)
+
+	assertEquals "did not use ruby specified in file" "$TEST_RUBY_ROOT" "$ruby_root"
 }
 
 test_chruby_exec()
