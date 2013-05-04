107b07f9aa9b650847fd1db60a823549be8aa7c1
Postmodern <postmodern.mod3@gmail.com>
2013-05-03 17:12:39 -0700

    Added an assert to test the PATH.

--- test/chruby_use_test.sh
+++ test/chruby_use_test.sh
@@ -2,6 +2,8 @@
 
 setUp()
 {
+	TEST_PATH="$PATH"
+
 	chruby_use $TEST_RUBY_ROOT
 }
 
@@ -11,8 +13,9 @@ test_chruby_use()
 	assertEquals "invalid RUBY_ENGINE"  "$TEST_RUBY_ENGINE" "$RUBY_ENGINE"
 	assertEquals "invalid RUBY_VERSION" "$TEST_RUBY_VERSION" "$RUBY_VERSION"
 	assertEquals "invalid GEM_ROOT"     "$TEST_RUBY_ROOT/lib/ruby/gems/$TEST_RUBY_API" "$GEM_ROOT"
-	assertEquals "invalid GEM_HOME"     "$HOME/.gem/$TEST_RUBY_ENGINE/$TEST_RUBY_VERSION" "$GEM_HOME"
+	assertEquals "invalid GEM_HOME"     "$TEST_GEM_HOME" "$GEM_HOME"
 	assertEquals "invalid GEM_PATH"     "$GEM_HOME:$GEM_ROOT" "$GEM_PATH"
+	assertEquals "invalid PATH"         "$TEST_GEM_HOME/bin:$TEST_GEM_ROOT/bin:$TEST_RUBY_ROOT/bin:$TEST_PATH" "$PATH"
 
 	assertEquals "could not find ruby in $PATH" "$TEST_RUBY_ROOT/bin/ruby" `which ruby`
 }
