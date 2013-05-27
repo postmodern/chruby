92934220a66f96ed3a1e3328d3d389459207b9ba
Postmodern <postmodern.mod3@gmail.com>
2013-05-26 20:07:24 -0700

    Use preexec_functions, which will execute in non-interactive mode.

--- share/chruby/auto.sh
+++ share/chruby/auto.sh
@@ -25,15 +25,14 @@ function chruby_auto() {
 }
 
 if [[ -n "$ZSH_VERSION" ]]; then
-	if [[ ! "$precmd_functions" == *chruby_auto* ]]; then
-		precmd_functions+=("chruby_auto")
+	if [[ ! "$preexec_functions" == *chruby_auto* ]]; then
+		preexec_functions+=("chruby_auto")
 	fi
 elif [[ -n "$BASH_VERSION" ]]; then
 	PROMPT_COMMAND="${PROMPT_COMMAND%% *}"
 
 	if [[ -n "$PROMPT_COMMAND" ]]; then
 		if [[ ! "$PROMPT_COMMAND" == *chruby_auto* ]]; then
-			PROMPT_COMMAND="${PROMPT_COMMAND%%;}"
 			PROMPT_COMMAND="${PROMPT_COMMAND%%;}; chruby_auto"
 		fi
 	else
