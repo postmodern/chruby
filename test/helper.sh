302408d3ff83a461995497e167172de0fae4103b
Josh McGee <josh.s.mcgee@gmail.com>
2013-07-28 00:55:35 -0700

    upcase SHUNIT2...again...for some reason...

--- test/helper.sh
+++ test/helper.sh
@@ -1,4 +1,4 @@
-[[ -z "$shunit2"     ]] && shunit2=/usr/share/shunit2/shunit2
+[[ -z "$SHUNIT2"     ]] && SHUNIT2=/usr/share/shunit2/shunit2
 [[ -n "$ZSH_VERSION" ]] && setopt shwordsplit
 
 . ./share/chruby/chruby.sh
