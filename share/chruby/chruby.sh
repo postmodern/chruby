2dd724c115c0091e6694edaaf7abccd714921472
Josh McGee <josh.s.mcgee@gmail.com>
2013-07-27 18:54:31 -0700

    fighting git

--- share/chruby/chruby.sh
+++ share/chruby/chruby.sh
@@ -1,9 +1,9 @@
 chruby_version="0.3.7"
 
-RUBIES=(
-  `find "$PREFIX"/opt/rubies -mindepth 1 -maxdepth 1 -type d 2>/dev/null`
-  `find "$HOME"/.rubies -mindepth 1 -maxdepth 1 -type d 2>/dev/null`
-)
+shopt -s nullglob
+for rubydir in "$PREFIX"/opt/rubies/* "$HOME"/.rubies/*; do
+  [[ -d "$rubydir/bin" ]] && RUBIES+=("$rubydir")
+done
 
 function chruby_reset()
 {
@@ -12,8 +12,8 @@ function chruby_reset()
 	PATH=":$PATH:" PATH="${PATH//:$RUBY_ROOT\/bin:/:}"
 
 	if [[ ! $UID -eq 0 ]]; then
-		[[ -n "$GEM_HOME" ]] && export PATH="${PATH//:$GEM_HOME\/bin:/:}"
-		[[ -n "$GEM_ROOT" ]] && export PATH="${PATH//:$GEM_ROOT\/bin:/:}"
+		[[ -n "$GEM_HOME" ]] && PATH="${PATH//:$GEM_HOME\/bin:/:}"
+		[[ -n "$GEM_ROOT" ]] && PATH="${PATH//:$GEM_ROOT\/bin:/:}"
 
 		GEM_PATH=":$GEM_PATH:"
 		GEM_PATH="${GEM_PATH//:$GEM_HOME:/:}"
@@ -41,11 +41,12 @@ function chruby_use()
 	RUBYOPT="$2"
 	PATH="$RUBY_ROOT/bin:$PATH"
 
-	eval "$("$RUBY_ROOT/bin/ruby" - <<EOF
+	eval "$(
+		"$RUBY_ROOT/bin/ruby" - <<EOF
 begin; require 'rubygems'; rescue LoadError; end
-puts "export RUBY_ENGINE=#{defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'}"
-puts "export RUBY_VERSION=#{RUBY_VERSION}"
-puts "export GEM_ROOT=#{Gem.default_dir.inspect}" if defined?(Gem)
+puts "export RUBY_ENGINE=#{defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'};"
+puts "export RUBY_VERSION=#{RUBY_VERSION};"
+puts "export GEM_ROOT=#{Gem.default_dir.inspect};" if defined?(Gem)
 EOF
 )"
 
