7a052ccb8a5a48304bea2b07c3e8df8d740fcd3c
Josh McGee <josh.s.mcgee@gmail.com>
2013-07-27 22:28:04 -0700

    indentation

--- share/chruby/auto.sh
+++ share/chruby/auto.sh
@@ -1,37 +1,37 @@
 unset ruby_auto_version
 
 function chruby_auto() {
-	local dir="$PWD"
-	local version_file
-	local version
+local dir="$PWD"
+local version_file
+local version
 
-	until [[ -z "$dir" ]]; do
-		version_file="$dir/.ruby-version"
+until [[ -z "$dir" ]]; do
+  version_file="$dir/.ruby-version"
 
-		if [[ -f "$version_file" ]]; then
-			read -r version < "$version_file"
+  if [[ -f "$version_file" ]]; then
+    read -r version < "$version_file"
 
-			if [[ "$version" == "$ruby_auto_version" ]]; then return
-			else
-				ruby_auto_version="$version"
-				chruby "$version"
-				return $?
-			fi
-		fi
+    if [[ "$version" == "$ruby_auto_version" ]]; then return
+    else
+      ruby_auto_version="$version"
+      chruby "$version"
+      return $?
+    fi
+  fi
 
-		dir="${dir%/*}"
-	done
+  dir="${dir%/*}"
+done
 
-	if [[ -n "$ruby_auto_version" ]]; then
-		chruby_reset
-		unset ruby_auto_version
-	fi
+if [[ -n "$ruby_auto_version" ]]; then
+  chruby_reset
+  unset ruby_auto_version
+fi
 }
 
 if [[ -n "$ZSH_VERSION" ]]; then
-	if [[ ! "$preexec_functions" == *chruby_auto* ]]; then
-		preexec_functions+=("chruby_auto")
-	fi
+  if [[ ! "$preexec_functions" == *chruby_auto* ]]; then
+    preexec_functions+=("chruby_auto")
+  fi
 elif [[ -n "$BASH_VERSION" ]]; then
-	trap '[[ "$BASH_COMMAND" != "$PROMPT_COMMAND" ]] && chruby_auto' DEBUG
+  trap '[[ "$BASH_COMMAND" != "$PROMPT_COMMAND" ]] && chruby_auto' DEBUG
 fi
