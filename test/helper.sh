1c043c101d53376a567ea0e0022f6ae624e1ee0d
Benoit Daloze <eregontp@gmail.com>
2020-03-28 15:59:42 +0100

    Fail early if missing the ruby to test

--- test/helper.sh
+++ test/helper.sh
@@ -13,6 +13,11 @@ test_ruby_version_x_y="${test_ruby_version%.*}"
 test_ruby_api="${TEST_RUBY_API:-${test_ruby_version%.*}.0}"
 test_ruby_root="$PWD/test/fixtures/opt/rubies/$test_ruby_engine-$test_ruby_version"
 
+if [[ "$(basename "$0")" != "setup" && ! -d "$test_ruby_root" ]]; then
+	echo "$test_ruby_root needs to exist, use test/setup or build a Ruby there"
+	exit 2
+fi
+
 test_path="$PATH"
 test_gem_home="$HOME/.gem/$test_ruby_engine/$test_ruby_version"
 test_gem_root="$test_ruby_root/lib/ruby/gems/$test_ruby_api"
