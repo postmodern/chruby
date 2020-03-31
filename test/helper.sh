1db2847bc02c8fc6caa3c3dd499ace71f5c071fc
Postmodern <postmodern.mod3@gmail.com>
2020-03-31 15:52:44 -0700

    Revert "Upgraded the default test version to 2.5.0 due to XCode/RVM compatibility issues."
    * RVM only provides up-to ruby-2.2.5 for macOS 10.12.
      https://rvm.io/binaries/osx/10.12/x86_64/

--- test/helper.sh
+++ test/helper.sh
@@ -8,7 +8,7 @@ export PATH="$PWD/bin:$PATH"
 . ./test/fixtures.sh
 
 test_ruby_engine="${TEST_RUBY_ENGINE:-ruby}"
-test_ruby_version="${TEST_RUBY_VERSION:-2.5.0}"
+test_ruby_version="${TEST_RUBY_VERSION:-2.2.5}"
 test_ruby_version_x_y="${test_ruby_version%.*}"
 test_ruby_api="${TEST_RUBY_API:-${test_ruby_version%.*}.0}"
 test_ruby_root="$PWD/test/fixtures/opt/rubies/$test_ruby_engine-$test_ruby_version"
