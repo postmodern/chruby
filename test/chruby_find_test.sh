0e78e9b2245cfadacf8bab743a6cc1de33f6845c
Postmodern <postmodern.mod3@gmail.com>
2020-09-23 21:43:31 -0700

    Moved some chruby matching tests into the chruby_find tests.

--- test/chruby_find_test.sh
+++ test/chruby_find_test.sh
@@ -1,21 +1,50 @@
 . ./test/helper.sh
 
+function setUp()
+{
+	test_ruby_path1="/path/to/ruby1"
+	test_ruby_path2="/path/to/ruby2"
+
+	RUBIES=("$test_ruby_path1" "$test_ruby_path2")
+}
+
 function test_chruby_find_with_exact_name()
 {
-	local exact_match="${test_ruby_root##*/}"
+	local exact_match="${test_ruby_path2##*/}"
 	local result="$(chruby_find "$exact_match")"
 
 	assertEquals "did not return the correct ruby dir" \
-		     "$test_ruby_root" "$result"
+		     "$test_ruby_path2" "$result"
+}
+
+function test_chruby_find_with_exact_name_but_there_are_multiple_matches()
+{
+	RUBIES=(
+		"$test_ruby_path1"
+		"${test_ruby_path1}-foo"
+		"$test_ruby_path2"
+		"${test_ruby_path2}-bar"
+	)
+
+	local result="$(chruby_find "${test_ruby_path2##*/}")"
+
+	assertEquals "did not use the exact match first" \
+		     "$test_ruby_path2" "$result"
 }
 
 function test_chruby_find_with_a_substring()
 {
-	local substring="$test_ruby_version"
-	local result="$(chruby_find "$substring")"
+	local result="$(chruby_find "2")"
 
 	assertEquals "did not return the correct ruby dir" \
-		     "$test_ruby_root" "$result"
+		     "$test_ruby_path2" "$result"
+}
+
+function test_chruby_find_when_there_are_multiple_matches()
+{
+	local result="$(chruby_find "ruby")"
+
+	assertEquals "did not use the last match" "$test_ruby_path2" "$result"
 }
 
 function test_chruby_find_when_cannot_find_match()
