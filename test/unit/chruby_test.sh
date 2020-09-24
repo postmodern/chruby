edbf9cba4b71ac45e91eff7d2318a396fd325e53
Postmodern <postmodern.mod3@gmail.com>
2020-09-23 21:43:31 -0700

    Moved some chruby matching tests into the chruby_find tests.

--- test/unit/chruby_test.sh
+++ test/unit/chruby_test.sh
@@ -44,26 +44,6 @@ function test_chruby_X_Y()
 	assertEquals "did not match $test_ruby_version_x_y" "$test_ruby_root" "$RUBY_ROOT"
 }
 
-function test_chruby_multiple_matches()
-{
-	local fake_ruby="/path/to/ruby-${test_ruby_version_x_y}"
-
-	RUBIES=("$fake_ruby" "$test_ruby_root")
-
-	chruby "${test_ruby_version_x_y}" >/dev/null
-
-	assertEquals "did not use the last match" "$test_ruby_root" "$RUBY_ROOT"
-}
-
-function test_chruby_exact_match_first()
-{
-	RUBIES=("$test_ruby_root" "${test_ruby_root}-rc1")
-
-	chruby "${test_ruby_root##*/}"
-
-	assertEquals "did not use the exact match" "$test_ruby_root" "$RUBY_ROOT"
-}
-
 function test_chruby_system()
 {
 	chruby "$test_ruby_version" >/dev/null
