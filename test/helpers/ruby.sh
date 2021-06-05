3aec096f41b97b729e77d0d0086450fbfdb6bcd4
Postmodern <postmodern.mod3@gmail.com>
2021-06-05 16:42:20 -0700

    Define test_ruby_api before test_gem_root.
    * Also query test_ruby_api from ruby by via `RbConfig::CONFIG["ruby_version"]`.

--- test/helpers/ruby.sh
+++ test/helpers/ruby.sh
@@ -5,6 +5,7 @@ if [[ -n "$TEST_RUBY_ROOT" ]]; then
 	eval "$(RUBYGEMS_GEMDEPS="" "$test_ruby_root/bin/ruby" - <<EOF
 puts "test_ruby_engine=#{Object.const_defined?(:RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'};"
 puts "test_ruby_version=#{RUBY_VERSION};"
+puts "test_ruby_api=#{RbConfig::CONFIG["ruby_version"]};"
 begin; require 'rubygems'; puts "test_gem_root=#{Gem.default_dir.inspect};"; rescue LoadError; end
 EOF
 )"
@@ -12,10 +13,10 @@ else
 	test_ruby_engine="${TEST_RUBY_ENGINE:-ruby}"
 	test_ruby_version="${TEST_RUBY_VERSION:-2.2.5}"
 	test_ruby_root="$PWD/test/fixtures/opt/rubies/$test_ruby_engine-$test_ruby_version"
+	test_ruby_api="${TEST_RUBY_API:-${test_ruby_version%.*}.0}"
 	test_gem_root="$test_ruby_root/lib/ruby/gems/$test_ruby_api"
 fi
 
 test_ruby_version_x_y="${test_ruby_version%.*}"
-test_ruby_api="${TEST_RUBY_API:-${test_ruby_version%.*}.0}"
 
 test_gem_home="$HOME/.gem/$test_ruby_engine/$test_ruby_version"
